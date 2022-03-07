b5f2fd4f5c29aec6d8f00926938fe21c3e790a1d
Andressa Leles <leles.andressa@gmail.com>
2022-03-07 18:04:08 -0300

    This is my commit, of my completed challeng

---
+++ script.js
@@ -0,0 +1,100 @@
+let aviso = document.querySelector('#aviso')
+let formulario = document.querySelector('form')
+
+let btnCalcular = document.querySelector('#btnCalcular')
+let btnLimpar = document.querySelector('#btnLimpar')
+
+// selecionar caixas de texto por id
+let cxNota1 = document.querySelector('#nota1')
+let cxNota2 = document.querySelector('#nota2')
+let cxNota3 = document.querySelector('#nota3')
+let cxNota4 = document.querySelector('#nota4')
+let cxMedia = document.querySelector('#media')
+let cxSituacao = document.querySelector('#situacao')
+
+// CALCULAR MEDIA
+function calcularMedia(n1, n2, n3, n4) {
+    return (n1 + n2 + n3 + n4) / 4
+}
+
+// DEFINIR SITUACAO FINAL COM BASE NA MEDIA
+function situacaoFinal(mediaFinal) {
+    let situacaoFinal = ''
+    
+    if (mediaFinal >= 7) {
+        situacaoFinal = 'Aprovado(a)'
+    } else if (mediaFinal <= 3) {
+        situacaoFinal = 'Reprovado(a)'
+    } else {
+        situacaoFinal = 'Recuperação'
+    }
+    return situacaoFinal
+}
+
+// FORMATAR A CAIXA DE SITUACAO FINAL
+function formatarSituacao(situacaoFinal) {
+    console.log('Situação Final ' + situacaoFinal)
+    switch(situacaoFinal) {
+
+        case 'Aprovado(a)':
+            cxSituacao.classList.remove('reprovado')
+            cxSituacao.classList.remove('recuperacao')
+            cxSituacao.classList.add('aprovado')
+            console.log('adicionar class aprovado')
+            break
+        
+        case 'Reprovado(a)':
+            cxSituacao.classList.remove('aprovado')
+            cxSituacao.classList.remove('recuperacao')
+            cxSituacao.classList.add('reprovado')
+            console.log('adicionar class reprovado')
+            break
+        
+        case 'Recuperação':
+            cxSituacao.classList.remove('aprovado')
+            cxSituacao.classList.remove('reprovado')
+            cxSituacao.classList.add('recuperacao')
+            console.log('adicionar class recuperacao')
+                break
+
+        default:
+            console.log('Situação Indefinida')
+    } // fim do switch case
+
+}
+
+// CALCULAR A MEDIA APOS O CLICK NO BOTAO
+btnCalcular.addEventListener('click', function(e) {
+    console.log('Calcular Média')
+// pegar o valor que esta dentro das caixas
+// usar metodo parseFloat p converter string p float
+    let nota1 = parseFloat(cxNota1.value)
+    let nota2 = parseFloat(cxNota2.value)
+    let nota3 = parseFloat(cxNota3.value)
+    let nota4 = parseFloat(cxNota4.value)
+    let media = calcularMedia(nota1, nota2, nota3, nota4)
+    
+    console.log(nota1)
+    console.log(nota2)
+    console.log(nota3)
+    console.log(nota4)
+    console.log(media)
+
+    if(isNaN(media) || media < 0) {
+        console.log("Não é um número")
+        cxSituacao.value = ''
+    } else {
+        cxMedia.value = parseFloat(media)
+        cxSituacao.value = situacaoFinal(media)
+        formatarSituacao(situacaoFinal(media))
+    }
+    e.preventDefault()
+})
+
+// APOS LIMPAR TIRAR AS CLASS DA CX SITUACAO
+btnLimpar.addEventListener('click', function() {
+    cxSituacao.classList.remove('aprovado')
+    cxSituacao.classList.remove('reprovado')
+    cxSituacao.classList.remove('recuperacao')
+})
+ 
